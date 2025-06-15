4232579882f1203745aeb7aaca0c572955fceda9
ArfanFahad <arfanfahad40@gmail.com>
2025-06-15 11:21:58 +0600

    refactor: change name

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
             <Link to="/contact">Contact</Link>
           </li>
           <li>
-            <Link to="/gallery">Gallery</Link>
+            <Link to="/Products">Products</Link>
           </li>
         </ul>
       </nav>
