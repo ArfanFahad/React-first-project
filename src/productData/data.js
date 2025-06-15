574f9e246976ebb8c346ebdef52962ad84269998
ArfanFahad <arfanfahad40@gmail.com>
2025-06-15 11:26:47 +0600

    dummy data

---
+++ src/productData/data.js
@@ -0,0 +1,23 @@
+export const electronics = [
+  {
+    id: 1,
+    name: "Smartphone",
+    brand: "Samsung",
+    price: 599,
+    img: "https://via.placeholder.com/150",
+  },
+  {
+    id: 2,
+    name: "Laptop",
+    brand: "Dell",
+    price: 999,
+    img: "https://via.placeholder.com/150",
+  },
+  {
+    id: 3,
+    name: "Bluetooth Speaker",
+    brand: "JBL",
+    price: 120,
+    img: "https://via.placeholder.com/150",
+  },
+];
