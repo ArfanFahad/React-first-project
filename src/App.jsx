5139978011363b63417e137c4d34f1d3e3bd2ace
ArfanFahad <arfanfahad40@gmail.com>
2025-06-15 11:21:21 +0600

    refactor: changed name of component

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import Gallery from "./pages/Gallery";
+import Products from "./pages/Products";
 import Footer from "./components/Footer";
 
 export default function App() {
@@ -15,7 +15,7 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/About" element={<About />} />
         <Route path="/Contact" element={<Contact />} />
-        <Route path="/Gallery" element={<Gallery />} />
+        <Route path="/Products" element={<Products />} />
       </Routes>
       <Footer />
     </>
